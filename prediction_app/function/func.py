e4912335a19f474185730cd4f1c239e78423d2fc
jinbyeong <parkjinb2@gmail.com>
2021-10-09 22:42:17 +0900

    commit

--- prediction_app/function/func.py
+++ prediction_app/function/func.py
@@ -26,3 +26,30 @@ def tomweather() :
             wind = np.append(wind, i['wind']['speed'])
             hum = np.append(hum, i['main']['humidity'])
     return {'temp' : round(temp.mean(),2), 'wind' : round(wind.mean(),2), 'hum' : round(hum.mean(),2)}
+
+def dbinsert(userlog) :
+    import psycopg2
+    import os
+    from dotenv import load_dotenv
+    import pandas as pd
+
+    load_dotenv()
+
+    host = os.environ.get('DB_HOST')
+    user = os.environ.get('DB_USER')
+    password = os.environ.get('DB_PASSWORD')
+    database = os.environ.get('DB_NAME')
+
+
+ 
+    df = pd.read_csv('data.csv')
+
+    conn = psycopg2.connect(database = database,host=host,password=password,user=user,port=5432)
+    cur = conn.cursor()
+    cur.execute("INSERT INTO userlog (log_date, p_temp, p_wind, p_hum, p_isholiday, p_count) VALUES (%s, %s, %s, %s, %s, %s);",(userlog[0],userlog[1],userlog[2],userlog[3],userlog[4],userlog[5]))
+	
+
+    conn.commit()
+    conn.close()
+    return print("complete!")
+
--- prediction_app/routes/result.py
+++ prediction_app/routes/result.py
@@ -8,15 +8,21 @@ resultbp=Blueprint('result',__name__)
 def index() : 
     if request.method == 'POST' :
         import joblib        
-        from prediction_app.function.func import tomweather
+        from prediction_app.function.func import tomweather,dbinsert
+        import datetime
+        
 
+        date = datetime.datetime.now()
+        log = str(date) 
         form_data = request.form
         
         f = open('./prediction_app/lgbm.pkl','rb')
         loaded_model = joblib.load(f)
         dic = tomweather()
-        input_data = [[dic['temp'],dic['wind'],dic['hum'],int(form_data['holiday'])]] # 1 자리에 휴일 여부 받아서 예측
+        input_data = [[dic['temp'],dic['wind'],dic['hum'],int(form_data['holiday'])]]
         result = loaded_model.predict(input_data)
         f.close()
+        userlog = [log,dic['temp'],dic['wind'],dic['hum'],int(form_data['holiday']),round(result[0])]
+        dbinsert(userlog)
         return render_template('result.html',prediction = round(result[0]))
         
