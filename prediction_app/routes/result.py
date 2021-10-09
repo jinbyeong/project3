b9e3cba82465f01ae26b813e5c8643aa04f61e8b
jinbyeong <parkjinb2@gmail.com>
2021-10-09 17:05:26 +0900

    commit

--- prediction_app/routes/result.py
+++ prediction_app/routes/result.py
@@ -7,13 +7,15 @@ resultbp=Blueprint('result',__name__)
 @resultbp.route('/result', methods =['POST','GET'])
 def index() : 
     if request.method == 'POST' :
-        import joblib
-        
+        import joblib        
         from prediction_app.function.func import tomweather
+
+        form_data = request.form
+        
         f = open('./prediction_app/lgbm.pkl','rb')
         loaded_model = joblib.load(f)
         dic = tomweather()
-        input_data = [[dic['temp'],dic['wind'],dic['hum'],1]] # 1 자리에 휴일 여부 받아서 예측
+        input_data = [[dic['temp'],dic['wind'],dic['hum'],int(form_data['holiday'])]] # 1 자리에 휴일 여부 받아서 예측
         result = loaded_model.predict(input_data)
         f.close()
         return render_template('result.html',prediction = round(result[0]))
